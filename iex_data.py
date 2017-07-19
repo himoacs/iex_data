7c501fc2256660f465aff33051c4ff4ce360e26f
himoacs <himoacs@gmail.com>
2017-07-18 20:20:58 -0400

    added methods to get more data from IEX

--- iex_data.py
+++ iex_data.py
@@ -45,11 +45,12 @@ class API(object):
 
         return [x for x in securities if x in set(valid_securities)]
 
-    def _url_to_dataframe(self, url):
+    def _url_to_dataframe(self, url, nest=None):
 
         """
         Takes a url and returns the response in a pandas dataframe
         :param url: str url
+        :param nest: column with nested data
         :return: pandas dataframe containing data from url
         """
 
@@ -58,7 +59,12 @@ class API(object):
         elevations = response.read()
         data = json.loads(elevations)
 
-        return pd.DataFrame(json_normalize(data))
+        if nest:
+            data = json_normalize(data[nest])
+        else:
+            data = json_normalize(data)
+
+        return pd.DataFrame(data)
 
     def get_latest_quote_and_trade(self, securities):
 
@@ -98,5 +104,106 @@ class API(object):
             df['time'] = pd.to_datetime(df['time'], unit='ms')
             df.set_index(['symbol'], inplace=True)
             return df
+        else:
+            print('These stock(s) are invalid!')
+
+    def get_latest_news(self, securities, count=1):
+
+        """
+        Get latest news for securities. By default, gets one news item per security.
+
+        :param securities: list of securities
+        :param count: how many news items to return
+        :return: dataframe
+        """
+
+        securities = self.return_valid_securities(securities)
+
+        final_df = pd.DataFrame({})
+
+        # Get news for each security and then append the results together
+        if securities:
+            for symbol in securities:
+                suffix = r'stock/{symbol}/news/last/{count}'.format(symbol=symbol,
+                                                                    count=count)
+                df = self._url_to_dataframe(self._end_point_prefix + suffix)
+                df['time'] = pd.to_datetime(df['datetime'])
+                del df['datetime']
+                df['symbol'] = symbol
+                df = df[['symbol', 'time', 'headline', 'summary', 'source', 'url', 'related']]
+                final_df = final_df.append(df, ignore_index=True)
+            return final_df
+        else:
+            print('These stock(s) are invalid!')
+
+    def get_financials(self, securities):
+
+        """
+        Get latest financials of securities. By default, gets one news item per security.
+        :param securities: list of symbols
+        :return: dataframe
+        """
+
+        securities = self.return_valid_securities(securities)
+
+        final_df = pd.DataFrame({})
+
+        # Get financials of each security and then append the results together
+        if securities:
+            for symbol in securities:
+                suffix = r'stock/{symbol}/financials'.format(symbol=symbol)
+                df = self._url_to_dataframe(self._end_point_prefix + suffix, 'financials')
+                df['symbol'] = symbol
+                final_df = final_df.append(df, ignore_index=True)
+            return final_df
+        else:
+            print('These stock(s) are invalid!')
+
+    def get_earnings(self, securities):
+
+        """
+        Get latest earnings for securities.
+        :param securities: list of symbols
+        :return: dataframe
+        """
+
+        securities = self.return_valid_securities(securities)
+
+        final_df = pd.DataFrame({})
+
+        # Get earnings data for each security and then append the results together
+        if securities:
+            for symbol in securities:
+                suffix = r'stock/{symbol}/earnings'.format(symbol=symbol)
+                df = self._url_to_dataframe(self._end_point_prefix + suffix, 'earnings')
+                df['symbol'] = symbol
+                final_df = final_df.append(df, ignore_index=True)
+            return final_df
+        else:
+            print('These stock(s) are invalid!')
+
+    def get_trade_bars_data(self, securities, bucket='1m'):
+
+        """
+        Get bucketed trade/volume data. Supported buckets are: 1m, 3m, 6m, 1y, ytd, 2y, 5y
+
+        :param securities: list of securities
+        :param bucket:
+        :return: dataframe
+        """
+
+        securities = self.return_valid_securities(securities)
+
+        final_df = pd.DataFrame({})
+
+        # Get earnings data for each security and then append the results together
+        if securities:
+            for symbol in securities:
+                suffix = r'stock/{symbol}/chart/{bucket}'.format(symbol=symbol,
+                                                                   bucket=bucket)
+                df = self._url_to_dataframe(self._end_point_prefix + suffix)
+                df['symbol'] = symbol
+                final_df = final_df.append(df, ignore_index=True)
+            return final_df
         else:
             print('These stock(s) are invalid!')
